75deeb549463eca584aa9211e270a8ae7ea817f1
yuta-t <suymsk@gmail.com>
2016-05-07 18:33:07 +0900

    create Article model add method get articles

--- rss-reader/Article.swift
+++ rss-reader/Article.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+import Alamofire
+import Fuzi
+import Bond
+
+struct Article {
+    let title: String
+    let url: String
+    let datetime: String
+    
+    static func getFromJin() -> ObservableArray<Article> {
+        let url = "http://jin115.com/index.rdf"
+        let articles: ObservableArray<Article> = []
+        
+        Alamofire.request(.GET, url)
+            .responseData { response in
+            guard let data = response.data else { return }
+            
+            guard let doc = try? XMLDocument(data: data) else { return }
+            
+            guard let root = doc.root else { return }
+            
+            for item in root.children(tag: "item") {
+                guard let title = item.firstChild(tag: "title"),
+                    url = item.firstChild(tag: "link"),
+                    datetime = item.firstChild(css: "dc:date") else { return }
+                
+                let article = Article(title: title.stringValue,
+                    url: url.stringValue,
+                    datetime: NSDate.convert(datetime.stringValue))
+                articles.append(article)
+            }
+        }
+        return articles
+    }
+}
